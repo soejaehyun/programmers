24afa7fd4fe869501846246c2501aa923838d508
jaehyun.seo <seojaehyun12@naver.com>
2023-04-16 21:11:36 +0900

    [LEVEL2] 멀쩡한 사각형 함수 이름 수정

--- LEVEL2/62048/main.c
+++ LEVEL2/62048/main.c
@@ -2,20 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-int gdc(int a, int b) {
+int gcd(int a, int b) {
     int num1, num2;
     
     if (a > b) { num1 = a; num2 = b; }
     else { num1 = b; num2 = a; }
 
     if (num2 == 0) return num1;
-    return gdc(num2, num1 % num2);
+    return gcd(num2, num1 % num2);
 }
 int main(void) {
     int w = 1, h = 1;
     int answer;
 
-    answer = w * h - (w + h - gdc(w, h));
+    answer = (long long)w * (long long)h - (w + h - gcd(w, h));
     printf("%d\n", answer);
     return 0;
 }
